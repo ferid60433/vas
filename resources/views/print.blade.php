a42f14ab0455cd4c01dc41ed43ecf7ded95d7942
Sam As End <4sam21@gmail.com>
2019-01-31 09:58:42 +0300

    minor bug fixed
    - Removed brands
    - Removed node from docker image
    - Absolute path for docker service

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Andegna Systems PLC">
+    <meta name="author" content="Call 0921631393">
     <link rel="icon" href="favicon.ico">
     <title>{{ $title or 'Home' }} | {{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="css/app.css">
